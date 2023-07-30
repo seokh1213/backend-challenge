6a67e358b5522e595c82df3700987396896c69cb
홍석현 <seokh1213@gmail.com>
2023-07-30 21:01:58 +0900

    feat: 주석 추가

--- modules/kafka/src/main/kotlin/forever/backend/kafka/partitioner/IntKeyPartitioner.kt
+++ modules/kafka/src/main/kotlin/forever/backend/kafka/partitioner/IntKeyPartitioner.kt
@@ -16,6 +16,9 @@ class IntKeyPartitioner : Partitioner {
         valueBytes: ByteArray?,
         cluster: Cluster
     ): Int {
+        // metadata가 즉시 즉시 반영이 안되어 오류가 발생할 수 있다.
+        // metadata.max.age.ms(기본값: 3000초, 5분)를 변경 해야 한다. 그래도 타이밍 이슈로 메세지 전송 실패 발생
+
         val numPartitions = cluster.partitionCountForTopic(topic) ?: 1
         return (key as String).toInt() % numPartitions
     }
